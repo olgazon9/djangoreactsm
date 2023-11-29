1353e63196bf167f5f3dd4fbb2d78148b3c9b209
olgazon9 <rfonderbrider@gmail.com>
2023-11-29 13:11:56 +0200

    cartworking

--- Front/jwt-auth-app/src/Checkout.jsx
+++ Front/jwt-auth-app/src/Checkout.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 const Checkout = ({ cart, setCart }) => {
     const [address, setAddress] = useState('');
 
+    // Calculate total price
+    const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
+
     const handleCheckout = async () => {
         const orderEndpoint = 'http://localhost:8000/api/orders/';
 
@@ -11,11 +14,13 @@ const Checkout = ({ cart, setCart }) => {
             const orderDetails = cart.map(item => ({
                 product: item.id,
                 quantity: item.quantity,
+                price: item.price,
             }));
 
             const order = {
                 order_details: orderDetails,
                 address: address, // Add other fields like address if needed
+                total_price: totalPrice, // Include total price in the order
             };
 
             const response = await axios.post(orderEndpoint, order);
@@ -35,6 +40,7 @@ const Checkout = ({ cart, setCart }) => {
                 Address:
                 <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
             </label>
+            <p>Total Price: ${totalPrice.toFixed(2)}</p>
             <button onClick={handleCheckout}>Place Order</button>
         </div>
     );
