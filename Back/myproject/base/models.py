d22deb33759e4bdcfd8333a86d9462269ff655c0
olgazon9 <rfonderbrider@gmail.com>
2023-11-29 14:27:43 +0200

    usercheckout

--- Back/myproject/base/models.py
+++ Back/myproject/base/models.py
@@ -1,5 +1,6 @@
 # models.py
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -12,17 +13,17 @@ class Product(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    # Other fields...
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)  # Replace '1' with the default user ID
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
 
 class OrderDetail(models.Model):
     order = models.ForeignKey(Order, related_name='order_details', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
     quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f'{self.order} - {self.product}'
-
 
 # Add a default value for the product field
 OrderDetail._meta.get_field('product').default = 1  # Replace '1' with the default product ID
--- Back/myproject/base/views.py
+++ Back/myproject/base/views.py
@@ -12,6 +12,7 @@ from .serializers import OrderSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 # views.py
 class OrderListCreateView(APIView):
@@ -64,6 +65,39 @@ class ProductListCreateView(generics.ListCreateAPIView):
         if category_id:
             return Product.objects.filter(category_id=category_id)
         return Product.objects.all()
+    
+    class OrderListCreateView(APIView):
+     permission_classes = [IsAuthenticated]  # Ensure only authenticated users can place orders
+
+    def post(self, request, *args, **kwargs):
+        # Assuming you have the order data in request.data and order_details data in detail_data
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            # Assign the authenticated user to the order
+            serializer.validated_data['user'] = request.user
+            order = serializer.save()
+
+            # Create OrderDetail instances
+            detail_data = request.data.get('order_details', [])
+            for detail in detail_data:
+                # Extract the product ID from detail
+                product_id = detail.pop('product', None)
+
+                if product_id:
+                    # Ensure product_id is an integer
+                    product_id = int(product_id)
+
+                    # Get or create the Product instance
+                    product, _ = Product.objects.get_or_create(id=product_id)
+
+                    # Create OrderDetail with the correct Product instance
+                    OrderDetail.objects.create(order=order, product=product, **detail)
+                else:
+                    # Handle the case where product_id is not provided
+                    return Response({'error': 'Product ID is required for each order detail.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
